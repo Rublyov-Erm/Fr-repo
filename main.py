36f4f5c52cc424869f7eb4d61266dea81b369d13
Rublyov-Erm <andrii.rublyov1@gmail.com>
2025-01-29 20:57:47 +0200

    First DZ

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-# This is a sample Python script.
+
+# from datetime import datetime
+#
+#
+# def get_days_from_today():
+#     while True:
+#         date = input("Input string YYYY-MM-DD")
+#         try:
+#             date = str(date)
+#             d = datetime.strptime(date, "%Y-%m-%d")
+#
+#             current_date = datetime.today()
+#             return current_date.toordinal() - d.toordinal()
+#
+#
+#         except ValueError:
+#             print(f"{date} wrong format try YYYY-MM-DD")
+#
+#
+# print(get_days_from_today())
+
+
+import random
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
